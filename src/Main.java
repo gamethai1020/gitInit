4758525c124a6bfe3ca2a671af97e7f36c9f0227
gamethai1020 <aussamaweelateh@gmail.com>
2018-10-18 12:01:14 +0700

    update src

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-
+import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Scanner inp = new Scanner(System.in);
+		System.out.print("Enter your name : ");
+		String name = inp.nextLine();
+		Hello name1 = new Hello();
+		name1.setName1(name);
+		System.out.println(name1.toString());
 	}
 
 }
