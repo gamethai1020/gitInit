2fa559f83644a35001e542106813aac76135cc30
saveyourbody <s5211_12@hotmail.com>
2018-10-18 11:40:46 -0700

    JavaProject HelloWorld

---
+++ src/Hello.java
@@ -0,0 +1,20 @@
+
+public class Hello {
+	String name;
+	public Hello() {
+		
+	}
+	
+	public void setName(String name) {
+		this.name =name; 
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public String toString() {
+		return 
+	}
+
+}
